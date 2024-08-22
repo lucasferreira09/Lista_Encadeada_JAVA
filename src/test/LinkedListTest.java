01ba29602e5609015d6d3daf3274133fd4953ed0
lucasferreira09 <148561715+lucasferreira09@users.noreply.github.com>
2024-08-21 23:42:11 -0300

    linked

---
+++ src/test/LinkedListTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import list.LinkedList;
+
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addElement(1);
+        list.addElement(2);
+        list.addElement(3);
+        list.addElement(4);
+        System.out.println(list);
+
+
+    }
+}
